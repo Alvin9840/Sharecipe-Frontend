b86b23dc2a754799f861dc7926ed0ea7b80deb99
Yi-Chen <alvin9840@gmail.com>
2025-09-21 15:53:34 -0400

    feat: integrate Sharecipe logo and update layout

--- app/(routes)/home/_components/Banner.js
+++ app/(routes)/home/_components/Banner.js
@@ -1,20 +1,27 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 function Banner() {
   return (
     <div className='p-2 bg-white-400 
     rounded-xl shadow-sm flex items-center
-    bg-blue-100
+    bg-orange-100
      border gap-5'>
-        <Image src='/social-panda-1.png' width={200} height={200} alt='panda'/>
-        <div>
-        <h2 className='font-bold text-[29px] '>WELCOME TO SOCIAL PANDA</h2>
+      <Image src='/Sharecipe.png' width={200} height={200} alt='panda' />
+      <div>
+        <h2 className='font-bold text-[29px] '>WELCOME TO Sharecipe</h2>
         <h2 className=''>Join Community, Create and Share your thought</h2>
-        <Button className="mt-3 bg-blue-500">Get Started</Button>
-        </div>
-        
+
+        <Link href='/sign-up'>
+          <Button className="mt-3 bg-orange-500">
+            Get Started
+          </Button>
+        </Link>
+
+      </div>
+
     </div>
   )
 }
